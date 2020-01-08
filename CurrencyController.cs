e3d481e1942ee3ba63eb53690af1fea235ee2501
Aji sahputra78 <59410960+ajisahputra78@users.noreply.github.com>
2020-01-09 00:33:05 +0700

    materi pemrograman lanjut amikom

---
+++ CurrencyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+class CurrencyController
+{
+    public string usdToIdr(string nominal)
+    {
+        var nominalDouble = Convert.ToDouble(nominal);
+        var result = nominalDouble * 15000;
+        return Convert.ToString(result);
+    }
+
+    public Boolean isAllowed(string nominal)
+    {
+        Regex regex = new Regex("[^0-9.-]+");
+        return !regex.IsMatch(nominal);
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace CurrencyApps
+{
+
+
+    public partial class MainWindow : Window
+    {
+        CurrencyController currencyController;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            currencyController = new CurrencyController();
+        }
+
+        private void Button_Hitung_Click(object sender, RoutedEventArgs e)
+        {
+            var nominalInput = InputTextBox.Text;
+            var result = "Invalid'";
+            if (currencyController.isAllowed(nominalInput))
+            {
+                result = currencyController.usdToIdr(nominalInput);
+            }
+            resultLabel.Content = result;
+
+        }
+    }
+}
+
+
